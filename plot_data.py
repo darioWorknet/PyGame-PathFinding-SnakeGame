6b35c48b099417050448645788303529ef677a55
Dario3Design <dario3design@gmail.com>
2021-01-07 12:33:53 +0100

    Uploaded CSV and .py data visualizer
    CSV contains 5000 game scores.
    Data visualizer shows a histogram of results

---
+++ plot_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+# FILE LOCATION
+main_path = "D:\Google Drive\PYTHON\SNAKE_GAME_A_STAR"
+file_name = 'scores_5000_15.csv'
+
+# LOAD DATAFRAME
+df = pd.read_csv(os.path.join(main_path, file_name))
+
+# PLOT DATAFRAME
+plt.figure(figsize=(16,8))
+plt.hist(df, bins=30)
+plt.show()
